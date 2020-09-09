5b22123069139db30e1c32f7be0b43335011f192
Joao Pedro de Melo <jpmn2013.terra.com.br@gmail.com.br>
2020-09-09 10:17:47 -0300

    Declaracao.c completa

--- Aulas/02-VariaveisConstantes/declaracao.c
+++ Aulas/02-VariaveisConstantes/declaracao.c
@@ -4,13 +4,17 @@ int numero = 100;
 char ch = 'A';
 unsigned char var = 10;
 const int SEXO_MASCULINO = 1;
+long maisde = 8000L;
 
 int main()
 {
 
-    printf("%d", numero);
-
-    printf("%c", ch);
-
+    printf("%d\n", numero);
+    printf("%ld\n", maisde);
+    printf("%c\n", ch);
+    printf("AAEE PORRA!\n");
+    
     return 0;
 }
+
+
